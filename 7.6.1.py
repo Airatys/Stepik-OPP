63247a4d5fa9b7b214fdba0ac36ab6c6827596fd
Airat Sharifullin <sharifullin.airat@yandex.ru>
2024-09-06 22:07:57 +0300

    stepik

---
+++ 7.6.1.py
@@ -0,0 +1,16 @@
+# С помощью наследования и приведенной ниже схемы постройте иерархию пустых классов:
+
+class A:
+    pass
+
+class B(A):
+    pass
+
+class C(A):
+    pass
+
+class D(A):
+    pass
+
+class E(B, D):
+    pass
