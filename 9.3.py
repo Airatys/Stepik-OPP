c3b6038921d882c88e63d20f39e5b54203e681c9
Airat Sharifullin <sharifullin.airat@yandex.ru>
2025-01-05 12:04:32 +0300

    stepik

---
+++ 9.3.py
@@ -0,0 +1,19 @@
+class CaesarCipher:
+    def __init__(self, n):
+        self.n = n
+        self.abcd = 'A B C D E F G H I J K L M N O P Q R S T U V W X Y Z'.split()
+
+    def encode(self):
+        pass
+
+    def decode(self):
+        pass
+
+
+
+
+
+cipher = CaesarCipher(10)
+print(cipher.encode('Beegeek'))
+print(cipher.decode('Gjjljjp'))
+
